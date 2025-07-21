bba5d818755b7e07704a7723acd7ac25c9f4a252
MMDHOP <mohammaddelavar1384@gmail.com>
2025-07-21 10:28:58 +0330

    category.html and category_style.css linked to urls

--- products/urls.py
+++ products/urls.py
@@ -5,12 +5,12 @@ urlpatterns = [
     path('category/',category_page),
     path('category/cleanser/',cleanser_page),
     path('category/Toner-Essence/',Toner_Essence_page),
-    path('category/Serum-Treatments',Serum_Treatments_page),
-    path('category/Moisturizer',Moisturizer_page),
-    path('category/Sunscreen',Sunscreen_page),
-    path('category/Exfoliator',Exfoliator_page),
-    path('category/Mask',Mask_page),
-    path('category/Eye-Care',Eye_Care_page),
+    path('category/Serum-Treatments/',Serum_Treatments_page),
+    path('category/Moisturizer/',Moisturizer_page),
+    path('category/Sunscreen/',Sunscreen_page),
+    path('category/Exfoliator/',Exfoliator_page),
+    path('category/Mask/',Mask_page),
+    path('category/Eye-Care/',Eye_Care_page),
 
 
 ]
