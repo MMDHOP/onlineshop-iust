112925da4e8c74aed6a1fb8db92e864ad3b574c5
MMDHOP <mohammaddelavar1384@gmail.com>
2025-07-22 10:42:55 +0330

    database connected and admin panel for users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # نسخه کپی از fieldsets پیش‌فرض
+    fieldsets = list(UserAdmin.fieldsets)
+    fieldsets[1] = (fieldsets[1][0], {'fields': fieldsets[1][1]['fields'] + ('user_type',)})
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_type'),  # 👈 فقط اینجا اضافه شد
+        }),
+    )
+
+    list_display = UserAdmin.list_display + ('email', 'user_type')
+    list_filter = UserAdmin.list_filter + ('user_type',)
