10d06d36eb776db7c5bfb55d5b1e790ae286e324
MMDHOP <mohammaddelavar1384@gmail.com>
2025-07-24 10:55:44 +0330

    models PRODUCTS created and added in admin panel

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('cleanser', 'cleanser'),
+        ('Toner-Essence', 'Toner-Essence'),
+        ('Serum-Treatments', 'Serum-Treatments'),
+        ('Moisturizer', 'Moisturizer'),
+        ('Sunscreen', 'Sunscreen'),
+        ('Exfoliator', 'Exfoliator'),
+        ('Mask', 'Mask'),
+        ('Eye-Care', 'Eye-Care'),
+    ]
+
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    brand = models.CharField(max_length=100, blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
