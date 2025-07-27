d685e95ed54bdc6c19fb7a30c744e164d650626a
MMDHOP <mohammaddelavar1384@gmail.com>
2025-07-28 01:57:59 +0330

    comment secion created without adding feature though

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
+from django.conf import settings
+
+from users.models import CustomUser 
 
 from multiselectfield import MultiSelectField
 class Product(models.Model):
@@ -127,3 +130,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comments(models.Model) :
+
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.created_at}"
