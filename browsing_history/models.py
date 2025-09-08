ac3d306c5e737d272ba93dd74f4177eb5da1ba34
MMDHOP <mohammaddelavar1384@gmail.com>
2025-09-08 20:34:26 +0330

    some changes in browsing history models

--- browsing_history/models.py
+++ browsing_history/models.py
@@ -2,23 +2,24 @@ from django.db import models
 from django.conf import settings
 from products.models import Product
 
-# Create your models here.
-
-
-
-class BrowsingHisrory(models.Model) :
+class BrowsingHistory(models.Model):
 
     INTERACTION_CHOICES = [
         ("view", "View"),
-        ("like", "Like"),
-        ("wishlist", "Wishlist"),
         ("cart", "Cart"),
+        ("wishlist", "Wishlist"),
+        ("like", "Like"),
+        ("rating", "Rating"),
     ]
 
-
-
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    interaction_type = models.CharField(max_length=20, choices=INTERACTION_CHOICES)
     timestamp = models.DateTimeField(auto_now_add=True)
-    interaction_type = models.CharField(max_length=20,choices=INTERACTION_CHOICES)
-    
+
+    class Meta:
+        verbose_name_plural = "Browsing Histories"
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.user} - {self.product} - {self.interaction_type}"
