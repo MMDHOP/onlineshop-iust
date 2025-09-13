85054d0bf45d233d55b0c014a167a25964386c6a
MMDHOP <mohammaddelavar1384@gmail.com>
2025-09-13 03:36:56 +0330

    adding timestamp to show in browsing history page

--- browsing_history/views.py
+++ browsing_history/views.py
@@ -29,18 +29,31 @@ def product_interaction(request, product_id):
         'product_histories': dict(product_histories)
     })
 
+
 @login_required
 def browsing_history_view(request):
     all_histories = BrowsingHistory.objects.filter(user=request.user).order_by('-timestamp')
     
     product_histories = defaultdict(list)
+    product_last_view = {} 
+
     for h in all_histories:
         if h.interaction_type not in product_histories[h.product]:
             product_histories[h.product].append(h.interaction_type)
-    
+        
+        if h.product not in product_last_view:
+            product_last_view[h.product] = h.timestamp
+            
+    histories_with_time = [
+        {'product': product, 'interactions': interactions, 'last_view': product_last_view[product]}
+        for product, interactions in product_histories.items()
+    ]
+
     return render(request, 'browsing_history.html', {
-        'product_histories': dict(product_histories)
+        'histories_with_time': histories_with_time,
     })
+
+
 
 
 @login_required
